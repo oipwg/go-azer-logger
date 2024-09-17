b47574c305f896012ec4f5d88f95d26d0811b478
Sky Young <skyoung@mediciland.com>
2024-09-17 15:04:12 -0600

    write log to file if `LOG_FILE` is set

--- runtime.go
+++ runtime.go
@@ -1,30 +1,59 @@
 package logger
 
 import (
-	"os"
+    "fmt"
+    "os"
 )
 
 var (
-	runtime *Runtime
-	muted   = &OutputSettings{}
-	verbose = &OutputSettings{
-		Info:  true,
-		Timer: true,
-		Error: true,
-	}
+    runtime *Runtime
+    muted   = &OutputSettings{}
+    verbose = &OutputSettings{
+        Info:  true,
+        Timer: true,
+        Error: true,
+    }
+    // Add a variable to hold the log file pointer
+    logFile *os.File
 )
 
 func init() {
-	runtime = &Runtime{
-		Writers: []OutputWriter{
-			NewStandardOutput(os.Stderr),
-		},
-	}
+    runtime = &Runtime{
+        Writers: []OutputWriter{
+            NewStandardOutput(os.Stderr),
+        },
+    }
+
+    // Check for LOG_FILE environment variable
+    logFilePath := os.Getenv("LOG_FILE")
+    if logFilePath != "" {
+        // Try to open the log file
+        var err error
+        logFile, err = os.OpenFile(logFilePath, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
+        if err != nil {
+            fmt.Fprintf(os.Stderr, "Failed to open log file %s: %v\n", logFilePath, err)
+            // If we can't open the file, continue without file logging
+        } else {
+            // Create a new StandardWriter for the file
+            fileWriter := NewStandardOutput(logFile)
+            fileWriter.(*StandardWriter).ColorsEnabled = false // Disable colors for file output
+
+            // Hook the file writer into the runtime
+            Hook(fileWriter)
+        }
+    }
+}
+
+// Add a function to close the log file when the application exits
+func CloseLogFile() {
+    if logFile != nil {
+        logFile.Close()
+    }
 }
 
 type OutputWriter interface {
-	Init()
-	Write(log *Log)
+    Init()
+    Write(log *Log)
 }
 
 type OutputSettings struct {
